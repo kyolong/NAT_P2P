1ed135ada9b2b7d55f416dbe0e11fa6714214e11
kyolong <39206821@qq.com>
2016-11-27 21:05:07 +0800

    逻辑已经做好,但是还是连不通

--- NATClient/NATClient.cpp
+++ NATClient/NATClient.cpp
@@ -3,8 +3,10 @@
 
 #include "stdafx.h"
 #include <Winsock2.h>
+#include <WS2tcpip.h>
 #include <stdio.h>
-
+#define  MAIN_PORT 33000
+#define SERVER_IP "127.0.0.1"
 int main()
 {
 	/////////////³õÊ¼»¯Ì×½Ó×Ö///////////////////////////////////////////////////
@@ -27,6 +29,266 @@ int main()
 		return -1;
 	}///...if
 	 ////////////////////////////////////////////////////////////
+
+	int ID = 0;
+	char serverip[128] = {'\0'};
+	printf("input ID\n");
+	scanf_s("%d", &ID);
+	getchar();
+	printf("input ip\n");
+	scanf_s("%s", serverip,16);
+	getchar();
+	SOCKET mainSock = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKADDR_IN mainAddr;
+	inet_pton(AF_INET, serverip, (void*)&(mainAddr.sin_addr));
+	mainAddr.sin_family = AF_INET;
+	mainAddr.sin_port = htons((u_short)MAIN_PORT);
+	connect(mainSock, (struct sockaddr*) &mainAddr,sizeof(mainAddr));
+	char  sendbuff[128] = {0};
+	int * tmp = (int*)sendbuff;
+	*tmp = htonl(ID);
+	send(mainSock, sendbuff, 4, 0);
+
+	char mode = 0;
+	while (true)
+	{
+		printf("input mode. s for server, c for client\n");
+		scanf_s("%c", &mode,1);
+		getchar();
+		if ('c' == mode || 's'== mode)
+		{
+			break;
+		}
+		
+	}
+	int flag = 1;
+	int len = sizeof(int);
+	SOCKET exSock = socket(AF_INET, SOCK_STREAM, 0);
+	setsockopt(exSock, SOL_SOCKET, SO_REUSEADDR, (char*)&flag, len);
+	SOCKADDR_IN exAddr;
+	inet_pton(AF_INET, serverip, (void*)&(exAddr.sin_addr));
+	exAddr.sin_family = AF_INET;
+	exAddr.sin_port = htons((u_short)(MAIN_PORT + 1));
+
+	SOCKADDR_IN addrLocal;
+	//addrLocal.sin_addr.S_un.S_addr = INADDR_ANY;
+	inet_pton(AF_INET, "192.168.16.104", (void*)&(addrLocal.sin_addr));
+	addrLocal.sin_family = AF_INET;
+	char  recvBuf[128];
+	int needRecv = 16;
+	int realRecv = 0;
+	int recvCnt = 0;
+	if ('c' == mode)
+	{
+		addrLocal.sin_port = htons((u_short)(MAIN_PORT + 2));
+
+		if (SOCKET_ERROR == bind(exSock, (SOCKADDR *)&addrLocal, sizeof(SOCKADDR)))
+		{
+			printf("bind faild");
+			closesocket(exSock);
+			return -1;
+		}
+		
+		connect(exSock, (struct sockaddr*) &exAddr, sizeof(exAddr));
+		tmp = (int*)sendbuff;
+		*tmp = htonl(ID);
+		send(exSock, sendbuff, 4, 0);
+
+		
+
+		int peerID = 0;
+		printf("input peer ID\n");
+		scanf_s("%d", &peerID);
+		getchar();
+
+		tmp = (int*)sendbuff;
+		*tmp = htonl(1);
+		tmp = (int*)(sendbuff+4);
+		*tmp = htonl(peerID);
+		send(mainSock, sendbuff, 8, 0);
+
+		needRecv = 12;
+		realRecv = 0;
+		recvCnt = 0;
+		while (needRecv > realRecv)
+		{
+			recvCnt = recv(mainSock, recvBuf + realRecv, needRecv - realRecv, 0);
+			if (0 > recvCnt)
+			{
+				printf("recv failed %s,%d\n", __FUNCTION__, __LINE__);
+				closesocket(mainSock);
+				return NULL;
+			}
+			realRecv += recvCnt;
+		}
+	
+		int *tmpID = (int*)recvBuf;
+		int peerIP = ntohl(*tmpID);
+		 tmpID = (int*)(recvBuf + 4);
+		int peerPort = ntohl(*tmpID);
+		 tmpID = (int*)(recvBuf + 8);
+		int requestID = ntohl(*tmpID);
+
+		SOCKADDR_IN addrPeer;
+		addrPeer.sin_addr.S_un.S_addr = htonl(peerIP);
+		addrPeer.sin_family = AF_INET;
+		addrPeer.sin_port = htons((u_short)peerPort);
+		char tmpIP[128] = { '\0' };
+		inet_ntop(AF_INET, (void*)&addrPeer.sin_addr, tmpIP, 128);
+		printf("to connect,ip:%s,port:%d,requestID:%d\n", tmpIP, peerPort, requestID);
+		
+		SOCKET exSock_ex = socket(AF_INET, SOCK_STREAM, 0);
+		setsockopt(exSock_ex, SOL_SOCKET, SO_REUSEADDR, (char*)&flag, len);
+		if (SOCKET_ERROR == bind(exSock_ex, (SOCKADDR *)&addrLocal, sizeof(SOCKADDR)))
+		{
+			printf("bind faild");
+			closesocket(exSock_ex);
+			return -1;
+		}
+		int cnt = 0;
+		while (SOCKET_ERROR == connect(exSock_ex, (struct sockaddr*) &addrPeer, sizeof(addrPeer)))
+		{
+			cnt++;
+			printf("reconnect %d\n", WSAGetLastError());
+			Sleep(500);
+			if (20 < cnt)
+			{
+				printf("failed to connect %d\n", WSAGetLastError());
+				break;
+			}
+		}
+		
+		char keychar;
+		while (true)
+		{
+			scanf_s("%c", &keychar,1);
+			getchar();
+			send(exSock_ex, &keychar, 1, 0);
+			Sleep(10);
+		}
+		
+		system("PAUSE");
+	}
+
+	if ('s' == mode)
+	{
+		addrLocal.sin_port = htons((u_short)(MAIN_PORT + 3));
+
+		if (SOCKET_ERROR == bind(exSock, (SOCKADDR *)&addrLocal, sizeof(SOCKADDR)))
+		{
+			printf("bind faild");
+			closesocket(exSock);
+			return -1;
+		}
+
+
+		 needRecv = 16;
+		 realRecv = 0;
+		 recvCnt = 0;
+		while (needRecv > realRecv)
+		{
+			recvCnt = recv(mainSock, recvBuf + realRecv, needRecv - realRecv, 0);
+			if (0 > recvCnt)
+			{
+				printf("recv failed %s,%d\n", __FUNCTION__, __LINE__);
+				closesocket(mainSock);
+				return NULL;
+			}
+			realRecv += recvCnt;
+		}
+		int *tmpID = (int*)recvBuf;
+		int peerID = ntohl(*tmpID);
+		 tmpID = (int*)(recvBuf+4);
+		int peerIP = ntohl(*tmpID);
+		 tmpID = (int*)(recvBuf+8);
+		int peerPort = ntohl(*tmpID);
+		 tmpID = (int*)(recvBuf+12);
+		int requestID = ntohl(*tmpID);
+
+		SOCKADDR_IN addrPeer;
+		addrPeer.sin_addr.S_un.S_addr = htonl(peerIP);
+		addrPeer.sin_family = AF_INET;
+		addrPeer.sin_port = htons((u_short)peerPort);
+		char tmpIP[128] = { '\0' };
+		inet_ntop(AF_INET, (void*)&addrPeer.sin_addr, tmpIP,128);
+		printf("ID:%d request connect,ip:%s,port:%d,requestID:%d\n", peerID, tmpIP, peerPort, requestID);
+		
+		connect(exSock, (struct sockaddr*) &exAddr, sizeof(exAddr));
+		tmp = (int*)sendbuff;
+		*tmp = htonl(ID);
+		send(exSock, sendbuff, 4, 0);
+		//closesocket(exSock);
+		SOCKET exSock_new = socket(AF_INET, SOCK_STREAM, 0);
+		setsockopt(exSock_new, SOL_SOCKET, SO_REUSEADDR, (char*)&flag, len);
+		if (SOCKET_ERROR == bind(exSock_new, (SOCKADDR *)&addrLocal, sizeof(SOCKADDR)))
+		{
+			printf("bind faild\n");
+			closesocket(exSock_new);
+			return -1;
+		}
+		printf("connect peer,fuck nat\n");
+		
+		int cnt = 0;
+		while (SOCKET_ERROR == connect(exSock_new, (struct sockaddr*) &addrPeer, sizeof(addrPeer)))
+		{
+			cnt++;
+			printf("refuck %d\n", WSAGetLastError());
+			Sleep(500);
+			if (2 < cnt)
+			{
+				
+				break;
+			}
+		}
+
+
+		printf("end fuck nat\n");
+		//closesocket(exSock);
+		SOCKET exSock_ex = socket(AF_INET, SOCK_STREAM, 0);
+		setsockopt(exSock_ex, SOL_SOCKET, SO_REUSEADDR, (char*)&flag, len);
+		if (SOCKET_ERROR == bind(exSock_ex, (SOCKADDR *)&addrLocal, sizeof(SOCKADDR)))
+		{
+			printf("bind faild");
+			closesocket(exSock_ex);
+			return -1;
+		}
+		if (SOCKET_ERROR == listen(exSock_ex, 100))
+		{
+			printf("listen faild");
+			closesocket(exSock_ex);
+			return NULL;
+		}
+		tmp = (int*)(sendbuff+8);
+		*tmp = htonl(requestID);
+		int ret = send(mainSock, sendbuff, 12, 0);
+		if (12 != ret)
+		{
+			printf("send failed\n");
+		}
+		SOCKET socketCommunication;
+		int len = sizeof(SOCKADDR);
+		
+
+
+			if (INVALID_SOCKET == (socketCommunication = accept(exSock_ex, NULL, NULL)))
+			{
+				printf("accept failderr:%d\n", WSAGetLastError());
+
+	
+			}
+			printf("success£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡\n");
+			char pr;
+			while (true)
+			{
+				recv(socketCommunication, &pr, 1, 0);
+				printf("%c", pr);
+				Sleep(10);
+			}
+	
+
+
+	}
+	
     return 0;
 }
 
--- NATServer/NATServer.cpp
+++ NATServer/NATServer.cpp
@@ -3,198 +3,155 @@
 
 #include "stdafx.h"
 #include <Winsock2.h>
+#include <WS2tcpip.h>
 #include <stdio.h>
 #include <list>
 
 #include "pthread.h"
+#include "P2PClient.h"
 using namespace std;
 
 
 #define MAIN_PORT 33000
-#define NEED_RECV_1 4;
-#define NEED_RECV_2 8;
-
-#define  REQUEST 1
-
-class CP2PRequest
-{
-	public:
-		int hostID;
-		int peerID;
-		int requestID;
-		CP2PRequest(int host,int peer)
-		{
-			hostID = host;
-			peerID = peer;
-			requestID = 0;
-
-		}
-		~CP2PRequest()
-		{
-
-		}
-};
-
-
-list<CP2PRequest*> requestList;
-
-class CP2PClient
-{
-public:
-	
-	SOCKET * mSocket;
-	pthread_t mCommunicationThreadProcID;
-	pthread_mutex_t mMutex;
-public:
-	int  CP2PClientRecvRegistInfo();
-
-	int  CP2PClientSendRequest(int host,int ip,int port,int id);
-
-	int  CP2PClientInitCommunicationThread();
-
-	static void * CP2PClientCommunicationThreadProc(void* pParam);
-
-	CP2PClient(SOCKET * sock);
-	~CP2PClient();
-
-public:
-	int mClientID;
-	int mClientIP;
-	int mClientPort;
-};
-
-list<CP2PClient*> clientList;//应该要加锁
-
-CP2PClient::CP2PClient(SOCKET * sock)
-{
-	mSocket = sock;
-	mClientID = 0;
-	mClientIP = 0;
-	mClientPort = 0;
-	
-}
 
 
 
-CP2PClient::~CP2PClient()
-{
-	delete mSocket;
-}
-
-
-int CP2PClient::CP2PClientRecvRegistInfo()
-{
-	char  recvBuf[128];
-	int needRecv = NEED_RECV_1;
-	int realRecv = 0;
-	int recvCnt = 0;
-	while (needRecv > realRecv)
-	{
-		recvCnt = recv(*mSocket, recvBuf + realRecv, needRecv - realRecv, 0);
-		if (0 > recvCnt)
-		{
-			printf("recv failed %s,%s\n", __FUNCTION__, __LINE__);
-			closesocket(*mSocket);
-			return -1;
-		}
-		realRecv += recvCnt;
-	}
-	int *tmpID = (int*)recvBuf;
-	mClientID = ntohl(*tmpID);
-	printf("ID:%d is registed\n", mClientID);
-	return 0;
-}
-
-int CP2PClient::CP2PClientSendRequest(int host, int ip, int port, int id)
-{
-
-	return 0;
-}
-
-int  CP2PClient::CP2PClientInitCommunicationThread()
-{
-	int ret = pthread_create(&mCommunicationThreadProcID, NULL, CP2PClientCommunicationThreadProc, this);
-	if (ret != 0)
-	{
-		printf("CP2PClientInitCommunicationThreadProc thread creat fail\n");
-		return -1;
-	}
-	pthread_detach(mCommunicationThreadProcID);
-	return 0;
-}
-
-void * CP2PClient::CP2PClientCommunicationThreadProc(void* pParam)
-{
-	printf("Enter %s \n", __FUNCTION__);
-		
-	CP2PClient *thisP2PClient = (CP2PClient*)pParam;
-
-	char  recvBuf[128];
-	int needRecv = NEED_RECV_2;
-	int realRecv = 0;
-	int recvCnt = 0;
-	while (needRecv > realRecv)
-	{
-		recvCnt = recv(*(thisP2PClient->mSocket), recvBuf + realRecv, needRecv - realRecv, 0);
-		if (0 > recvCnt)
-		{
-			printf("recv failed %s,%s\n", __FUNCTION__, __LINE__);
-			closesocket(*(thisP2PClient->mSocket));
-			return NULL;
-		}
-		realRecv += recvCnt;
-	}
-	
-	
-	int *tmpCmd = (int*)recvBuf;
-	int *tmpVal= (int*)(recvBuf+4);
-	int Cmd = ntohl(*tmpCmd);
-	int Val = ntohl(*tmpVal);
-	CP2PRequest * requst = NULL;
-	printf("[ID:%d] cmd is :%d,val is : %d\n", thisP2PClient->mClientID, Cmd, Val);
-	switch (Cmd)
-	{
-	case REQUEST:
-		requst = new CP2PRequest(thisP2PClient->mClientID, Val);
-		requestList.push_back(requst);
-		break;
-	default:
-		break;
-	}
 
 
 
-	printf("Exit %s \n", __FUNCTION__);
-	return NULL;
-}
+
+
+
+
+
 
 void * requestHandleThreadProc(void *)
 {
 	static int requestID = 0;
 
 	CP2PRequest * requst = NULL;
+	CP2PClient* hostClient = NULL;
+	CP2PClient* peerClient = NULL;
 	while (true)
 	{
 
-		if (0 < requestList.size())
+		if (0 < getRequstList()->size())
 		{
 			requestID++;
-			requst = requestList.front();
-			requestList.pop_front();
+			requst = getRequstList()->front();
+			getRequstList()->pop_front();
 			requst->requestID = requestID;
+			hostClient = NULL;
+			peerClient = NULL;
 			std::list<CP2PClient*>::iterator it;
-			for (it=clientList.begin();it!=clientList.end();it++)
+			for (it=getClientList()->begin();it!= getClientList()->end();it++)
 			{
 				if ((*it)->mClientID == requst->peerID)
 				{
-					
-				}
-				
-			}
+					peerClient = *it;
 
+				}
+				if ((*it)->mClientID == requst->hostID)
+				{
+					hostClient = *it;
+
+				}
+			}
+			if (NULL== hostClient || NULL== peerClient)
+			{
+				printf("offline \n;");
+			}
+			else
+			{
+				peerClient->CP2PClientSendRequest(hostClient->mClientID, hostClient->mClientIP, hostClient->mClientPort, requestID);
+			}
+			while (0 == peerClient->mStatus)
+			{
+				Sleep(100);
+			}
+			int ret = peerClient->CP2PClientRecvReady();
+			if (requestID != ret)
+			{
+				printf("request id error!!!!!!!!!!\n");
+				printf("request id error!!!!!!!!!!\n");
+				printf("request id error!!!!!!!!!!\n");
+			}
+			hostClient->CP2PClientSendReady(peerClient->mClientIP,peerClient->mClientPort, requestID);
+			
+			
 		}
 		
 		Sleep(1);
+	}
+	return NULL;
+}
+void* extendPortThreadProc(void * mParam)
+{
+	char ip[128] = {0};
+	SOCKET socketEX;
+	SOCKET socketCommunication;
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr = INADDR_ANY;
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons((u_short)(MAIN_PORT+1));
+	socketEX = socket(AF_INET, SOCK_STREAM, 0);
+	if (SOCKET_ERROR == bind(socketEX, (SOCKADDR *)&addrSrv, sizeof(SOCKADDR)))
+	{
+		printf("bind faild");
+		closesocket(socketEX);
+		return NULL;
+	}
+
+	if (SOCKET_ERROR == listen(socketEX, 100))
+	{
+		printf("listen faild");
+		closesocket(socketEX);
+		return NULL;
+	}
+	SOCKADDR_IN addrPeer;
+	int len = sizeof(SOCKADDR);
+	while (1)
+	{
+		
+		if (INVALID_SOCKET == (socketCommunication = accept(socketEX, (SOCKADDR *)&addrPeer, &len)))
+		{
+			printf("accept failderr:%d\n", WSAGetLastError());
+			continue;
+		}
+		printf("new ex connect!\n");
+		char  recvBuf[128];
+		int needRecv = 4;
+		int realRecv = 0;
+		int recvCnt = 0;
+		while (needRecv > realRecv)
+		{
+			recvCnt = recv(socketCommunication, recvBuf + realRecv, needRecv - realRecv, 0);
+			if (0 > recvCnt)
+			{
+				printf("recv failed %s,%d\n", __FUNCTION__, __LINE__);
+				closesocket(socketCommunication);
+				return NULL;
+			}
+			realRecv += recvCnt;
+		}
+		int *tmpID = (int*)recvBuf;
+		int ID  = ntohl(*tmpID);
+		
+
+		std::list<CP2PClient*>::iterator it;
+		for (it = getClientList()->begin(); it != getClientList()->end(); it++)
+		{
+			if ((*it)->mClientID == ID)
+			{
+				(*it)->mClientIP =  ntohl(addrPeer.sin_addr.S_un.S_addr) ;
+				(*it)->mClientPort = ntohs(addrPeer.sin_port);
+				inet_ntop(AF_INET, (void*)&addrPeer.sin_addr, ip,128);
+				printf("ID:%d is update ip:%s port:%d\n", ID, ip, (*it)->mClientPort);
+			}
+
+		}
+
+
 	}
 	return NULL;
 }
@@ -252,6 +209,15 @@ int main()
 		return -1;
 	}
 	pthread_detach(requestHandleThreadID);
+
+	pthread_t extendPortThreadID;
+	 ret = pthread_create(&extendPortThreadID, NULL, extendPortThreadProc, NULL);
+	if (ret != 0)
+	{
+		printf("extendPortThreadProc thread creat fail\n");
+		return -1;
+	}
+	pthread_detach(extendPortThreadID);
 	
 	CP2PClient* client = NULL;
 	while (1)
@@ -259,15 +225,15 @@ int main()
 		socketCommunication = new SOCKET;
 		if (INVALID_SOCKET == (*socketCommunication = accept(socketMain, (SOCKADDR *)NULL, NULL)))
 		{
-			printf("listen failderr:%d\n",WSAGetLastError());
+			printf("accept failderr:%d\n",WSAGetLastError());
 			delete socketCommunication;
 			continue;
 		}
 		printf("new connect!\n");
 		client = new CP2PClient(socketCommunication);
-		if (0 == client->CP2PClientRecvRegistInfo())
+		if (0 == client->CP2PClientRecvRegistInfo())//最好做成异步
 		{
-			clientList.push_back(client);
+			getClientList()->push_back(client);
 			client->CP2PClientInitCommunicationThread();
 		}
 		else
